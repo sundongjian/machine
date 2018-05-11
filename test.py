2b509028662d34a5462acfe24fcc24e267e9645e
sdj <xiaobomentu@163.com>
2018-05-11 19:32:32 +0800

    add fuzhi

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
-#
-# import numpy
-# from parsedata.parsedata import ParseData
-# # aggclassest = numpy.mat(numpy.zeros((10, 1)))
-# # print( numpy.mat(aggclassest)*numpy.mat(aggclassest).T)
-# a=[[0,2],[0,2],[0,0],[1,2],[1,2]]
-# # print(numpy.mat(a))
-# #file_path=r'F:\machine\file\hyxd_movie_01newdata4.xlsx'
-# #da = ParseData(file_path)
-# #data = da.dataset()
-#
-#
-# b=numpy.array(a)
-# print(numpy.nonzero())
-# ma=b[numpy.nonzero(b[:,1]>0)[0],:]
-# print(ma)
+
+import numpy
+from parsedata.parsedata import ParseData
+# aggclassest = numpy.mat(numpy.zeros((10, 1)))
+# print( numpy.mat(aggclassest)*numpy.mat(aggclassest).T)
+a=[[0,2],[0,2],[0,0],[1,2],[1,2]]
+# print(numpy.mat(a))
+#file_path=r'F:\machine\file\hyxd_movie_01newdata4.xlsx'
+#da = ParseData(file_path)
+#data = da.dataset()
+
+
+b=numpy.array(a)
+print(numpy.nonzero())
+ma=b[numpy.nonzero(b[:,1]>0)[0],:]
+print(ma)
 
 
 l=[1,2,3,4]
--- unsupervised/apriori/test.py
+++ unsupervised/apriori/test.py
@@ -1,11 +1,12 @@
-from unsupervised.apriori.get_apriori import apriori,apriorigen
+from unsupervised.apriori.get_apriori import apriori,generateRules
 
 
 
 #
-dataset=[[1,2,3],[23,35,56,],[34,56,78,],[23,45,78,],[12,45,89,],[12,56,17],[34,67,89,],[12,34,12],[16,47,89,],[45,78,89]]
+dataset=[[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
 a,b=apriori(dataset)
 print(a,b)
+generateRules(a, b, minConf=0.1)
 # freq=frozenset([1,2,3,5,6,7])
 # H1 = [frozenset([item]) for item in freq]
 # print(H1)
